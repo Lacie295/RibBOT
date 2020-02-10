259c587521b7441143db52208d8b6a4801fd1a5c
Sami <sami.bosch@gmail.com>
2020-02-10 15:35:40 +0100

    Bot coded

---
+++ Bot/db_handler.py
@@ -0,0 +1,94 @@
+# created by Sami Bosch on Wednesday, 10 February 2020
+
+# This file contains all functions necessary to handle the database
+
+import os
+import json
+
+songs = '../dates.json'
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, songs)
+if not os.path.exists(filename):
+    with open(filename, "w+") as f:
+        json.dump({"birthdays": {}, "channel": None, "events": {}}, f)
+        f.truncate()
+        f.close()
+
+with open(filename, "r+") as f:
+    db = json.load(f)
+    f.close()
+
+if "birthdays" not in db:
+    db['birthdays'] = {}
+
+if "channel" not in db:
+    db['channel'] = None
+
+if "events" not in db:
+    db['events'] = {}
+
+
+def write():
+    with open(filename, "w+") as file:
+        json.dump(db, file)
+        file.truncate()
+        file.close()
+
+
+def add_event(name, date):
+    datestr = str(date.day) + "/" + str(date.month) + "/" + str(date.year)
+    db['events'][name] = datestr
+    write()
+
+
+def remove_event(name):
+    if name in db['events']:
+        db['events'].pop(name)
+        write()
+
+
+def get_event_date(name):
+    if name in db['events']:
+        return db['events'][name]
+    else:
+        return None
+
+
+def get_events():
+    return db['events'].keys()
+
+
+def add_birthday(name, date):
+    datestr = str(date.day) + "/" + str(date.month)
+    if datestr in db['birthdays']:
+        db['birthdays'][datestr].append(name)
+    else:
+        db['birthdays'][datestr] = [name]
+    write()
+
+
+def remove_birthday(name, date):
+    datestr = str(date.day) + "/" + str(date.month)
+    if datestr in db['birthdays'] and name in db['birthdays'][datestr]:
+        if len(db['birthdays'][datestr]) == 1:
+            db['birthdays'].pop(datestr)
+        else:
+            db['birthdays'][datestr].pop(name)
+        write()
+
+
+def get_birthdays(date):
+    datestr = str(date.day) + "/" + str(date.month)
+    if datestr in db['birthdays']:
+        return db['birthdays'][datestr]
+    else:
+        return None
+
+
+def set_channel(gid, cid):
+    db['channel'] = (gid, cid)
+    write()
+
+
+def get_channel():
+    return db['channel']
--- Bot/message_parser.py
+++ Bot/message_parser.py
@@ -1,14 +1,152 @@
 # created by Sami Bosch on Wednesday, 27 November 2019
 
 # This file contains all functions necessary to reply to messages
+from asynctimer import AsyncTimer
+import db_handler
 
 from discord.ext import commands
+from datetime import datetime, timedelta, date
 
 
 def init(client):
-    class Example(commands.Cog):
-        @commands.command(pass_context=True)
-        async def example(self, context):
-            m = context.message
+    async def birthday(context, add=True):
+        m = context.message
+        if m.author.guild_permissions.administrator:
+            split = m.content.split(" ")
+            if len(split) > 2:
+                i = 1
+                s = ""
+                f = ""
+                while i < len(split):
+                    name = split[i]
+                    bday = split[i + 1]
+                    try:
+                        birth_date = datetime.strptime(bday, "%d/%m").date()
+                        print(birth_date)
+                        if add:
+                            db_handler.add_birthday(name, birth_date)
+                        else:
+                            db_handler.remove_birthday(name, birth_date)
+                        s += name + ", "
+                    except ValueError:
+                        f += name + ", "
+                    i += 2
+                s = "Successfully added birthdays for " + s[:-2] + ". " if s != "" else ""
+                f = "Failed to add for " + f[:-2] + "." if f != "" else ""
+                await context.send(s + f)
+            else:
+                await context.send("Please provide at least one pair.")
+        else:
+            await context.send("You don't have permissions for that!")
 
-    client.add_cog(Example())
+    class Birthdays(commands.Cog):
+        @commands.command(pass_context=True)
+        async def add_birthday(self, context):
+            """Adds a birthday for an AC character. You can give a list paired by name-date to add multiple. Date must be dd/mm.
+            Usage: %add_birthday name date"""
+            await birthday(context)
+
+        @commands.command(pass_context=True)
+        async def remove_birthday(self, context):
+            """Removes a birthday for an AC character. You can give a list paired by name-date to add multiple. Date must be dd/mm.
+            Usage: %remove_birthday name date"""
+            await birthday(context, add=False)
+
+    class Events(commands.Cog):
+        @commands.command(pass_context=True)
+        async def add_event(self, context):
+            """Schedules an event. Date must be dd/mm/yyyy.
+            Usage: %add_event name date"""
+            m = context.message
+            if m.author.guild_permissions.administrator:
+                split = m.content.split(" ")
+                if len(split) == 3:
+                    name = split[1]
+                    event_date = split[2]
+                    try:
+                        edate = datetime.strptime(event_date, "%d/%m/%Y").date()
+                        db_handler.add_event(name, edate)
+                        await context.send("Added event.")
+                    except ValueError:
+                        await context.send("Wrong date format.")
+                else:
+                    await context.send("Please provide a name and a date.")
+            else:
+                await context.send("You don't have permissions for that!")
+
+        @commands.command(pass_context=True)
+        async def remove_event(self, context):
+            """Removes an event.
+            Usage: %add_event name"""
+            m = context.message
+            if m.author.guild_permissions.administrator:
+                split = m.content.split(" ")
+                if len(split) == 2:
+                    name = split[1]
+                    db_handler.remove_event(name)
+                    await context.send("Removed event.")
+                else:
+                    await context.send("Please provide a name.")
+            else:
+                await context.send("You don't have permissions for that!")
+
+    class Utility(commands.Cog):
+        @commands.command(pass_context=True)
+        async def set_channel(self, context):
+            """Sets the channel for events and bdays.
+            Usage: %set_channel"""
+            m = context.message
+            if m.author.guild_permissions.administrator:
+                cid = m.channel.id
+                gid = m.guild.id
+                db_handler.set_channel(gid, cid)
+                await context.send("Successfully configured channel!")
+            else:
+                await context.send("You don't have permissions for that!")
+
+    client.add_cog(Birthdays())
+    client.add_cog(Events())
+    client.add_cog(Utility())
+
+    def secs():
+        x = datetime.today()
+        x_temp = x.replace(hour=15, minute=32, second=0, microsecond=0)
+        y = x_temp if x_temp > x else x_temp + timedelta(days=1)
+        delta_t = y - x
+        return delta_t.seconds + 1
+
+    async def send_events():
+        s = secs()
+        AsyncTimer(s, send_events)
+
+        coming = ""
+        now = ""
+
+        remove = []
+
+        events = db_handler.get_events()
+        for event in events:
+            edate = datetime.strptime(db_handler.get_event_date(event), "%d/%m/%Y").date()
+            today = date.today()
+            delta = edate - today
+            if delta.days == 0:
+                now += "Tapahtuma tänään: " + event + "\n"
+                remove.append(event)
+            else:
+                coming += str(delta.days) + " päivää tapahtumaan: " + event + "\n"
+
+        for event in remove:
+            db_handler.remove_event(event)
+
+        s = now + "\n" + coming + "\n"
+
+        birthdays = db_handler.get_birthdays(date.today())
+        for name in birthdays:
+            s += "Tänään syntymäpäivää viettää " + name + "\n"
+
+        gid, cid = db_handler.get_channel()
+        guild = client.get_guild(gid)
+        channel = guild.get_channel(cid)
+        await channel.send(s)
+
+    AsyncTimer(secs(), send_events)
